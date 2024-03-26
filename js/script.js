e91158858e642917b73d0b916c932db7d5e0aa55
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-26 01:13:17 +0000

    cube site

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 // Created by: Kai Nguyen
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
-function myButtonClicked() {
-  document.getElementById("answer").innerHTML = "<p>Hello, World!</p>"
+function findVolumeClicked() {
+  const lengthInput = parseInt(document.getElementById("length-entered").value)
+  const totalVolume = lengthInput ** 3 
+  document.getElementById("answer").innerHTML = "The volume is: " + totalVolume.toFixed(2) + " cm<sup>3"
 }
+
