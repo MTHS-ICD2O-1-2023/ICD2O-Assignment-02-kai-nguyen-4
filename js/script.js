451a394647576672d439a2c6cce5de27adc011fe
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-26 18:35:19 +0000

    .

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
 function findVolumeClicked() {
-  const lengthInput = parseInt(document.getElementById("length-entered").value)
+  const lengthInput = parseFloat(document.getElementById("length-entered").value)
   const totalVolume = lengthInput ** 3 
   document.getElementById("answer").innerHTML = "The volume is: " + totalVolume.toFixed(2) + " cm<sup>3"
 }
